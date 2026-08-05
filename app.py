5f9e42023ac58760aa40b7b7fc7379f836e48ad5
sulkantisiri0424-lang <sulkantisiri0424@gmail.com>
2026-08-05 16:53:37 +0530

    Connect FastAPI with dashboard

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 app = FastAPI(
     title="Autonomous Portfolio Rebalancing Agent",
     version="1.0.0"
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to Autonomous Portfolio Rebalancing Agent",
-        "status": "Running Successfully"
+        "message": "Autonomous Portfolio Rebalancing Agent is Running"
     }
+
+
+@app.get("/dashboard")
+def dashboard(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
 
 @app.get("/health")
 def health():
