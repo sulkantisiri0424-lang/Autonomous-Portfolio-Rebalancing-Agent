e46d3dfab494bd950493da4af5593cb852666bbc
sulkantisiri0424-lang <sulkantisiri0424@gmail.com>
2026-08-05 16:28:49 +0530

    Add configuration file

---
+++ config.py
@@ -0,0 +1,15 @@
+APP_NAME = "Autonomous Portfolio Rebalancing Agent"
+
+VERSION = "1.0.0"
+
+DATABASE = "portfolio.db"
+
+DRIFT_THRESHOLD = 5
+
+TARGET_ALLOCATION = {
+    "AAPL": 25,
+    "MSFT": 25,
+    "GOOGL": 20,
+    "AMZN": 15,
+    "NVDA": 15
+}
